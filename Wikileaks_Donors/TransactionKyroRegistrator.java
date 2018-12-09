fdc0476944208841dcf9f878bceaccbc50579712
Andria Njoku <andrianjoku@gmail.com>
2018-12-09 15:00:52 +0000

    Serialized remaining POJOS

--- Wikileaks_Donors/TransactionKyroRegistrator.java
+++ Wikileaks_Donors/TransactionKyroRegistrator.java
@@ -5,7 +5,9 @@ import org.apache.spark.serializer.KryoRegistrator;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.serializers.FieldSerializer;
 
+import POJO.TransactionInWritable;
 import POJO.TransactionOutWritable;
+import POJO.TransactionsJoined;
 
 
 
@@ -13,12 +15,12 @@ public class TransactionKyroRegistrator {
 
 	public void registerClasses(Kryo kryo) {
 	
-		// TODO Auto-generated method stub
-		
-		
 		//register class with Kyro for serialisation 
 		
 		kryo.register(TransactionOutWritable.class, new FieldSerializer(kryo, TransactionOutWritable.class));
+		kryo.register(TransactionInWritable.class,new FieldSerializer(kryo, TransactionInWritable.class));
+		kryo.register(TransactionsJoined.class, new FieldSerializer<>(kryo, TransactionsJoined.class));
+		
 		
 	}
 

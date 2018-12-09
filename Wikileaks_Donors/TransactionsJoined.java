9901c2b423c2b665912578b301708480c6106109
Andria Njoku <andrianjoku@gmail.com>
2018-12-09 13:08:10 +0000

    New Transaction Object
    This POJO will hold information from both side, vout and vin, this will
    make sorting easier.

---
+++ Wikileaks_Donors/TransactionsJoined.java
@@ -0,0 +1,18 @@
+package Bitcoin.Wikileaks_Donors;
+
+import POJO.TransactionInWritable;
+import POJO.TransactionOutWritable;
+
+public class TransactionsJoined {
+	
+	
+	public TransactionsJoined(TransactionInWritable tin, TransactionOutWritable tout) {
+		
+		
+		
+	}
+	
+	
+	
+
+}
